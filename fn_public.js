5c749f3113d9f626741b42335e02943524e17cd3
KingTiger1016 <benny4862456@gmail.com>
2022-03-04 14:01:54 +0800

    加入呼叫ajax

--- fn_public.js
+++ fn_public.js
@@ -6,3 +6,24 @@ function hostip() {
         return "https://hq.atl-lab.com.tw";
     }
 }
+
+// 共用_呼叫ajax
+function ajaxPostData(name, data) {
+    data = JSON.stringify(data)
+    data = data.replaceAll('{', ' ');
+    data = data.replaceAll('}', ' ');
+    // data = data.replaceAll('"', ' ');
+    data = encodeURI(data, "UTF-8");
+    // data = encodeURI(data,"UTF-8");
+    try {
+        $.ajax({
+            url: name + "?OpenAgent",
+            dataType: "jsonp",
+            data: data,
+            contentType: "application/x-www-form-urlencoded; charset=utf-8",
+            type: "POST"
+        })
+    } catch (err) {
+        alert(err.message)
+    }
+}
